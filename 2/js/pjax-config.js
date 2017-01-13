8714b663686a16e8bd518e99328e1641fef12035
muranaka <formail@java9.com>
2017-01-13 18:00:03 +0900

    1800

--- 2/js/pjax-config.js
+++ 2/js/pjax-config.js
@@ -8,17 +8,15 @@
   });
 
   $(document).bind('pjax:fetch', function() {
-    TweenMax.to('#container', 0.6, {
-      top: '60px',
-      opacity: '0',
+    TweenMax.to('.container_overlay', 0.6, {
+      width: '100%',
       ease: Power1.easeOut, y: 0
     });
   });
 
   $(document).bind('pjax:render', function() {
-    TweenMax.to('#container', 0.6, {
-      top: '50px',
-      opacity: '1',
+    TweenMax.to('.container_overlay', 0.6, {
+      width: '100%',
       ease: Power1.easeIn, y: 0
     });
   });
