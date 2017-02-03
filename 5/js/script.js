20a3960c848ee6270b1075fe8ea16f8ecabaddf8
muranaka <formail@java9.com>
2017-02-03 17:54:35 +0900

    5431

--- 5/js/script.js
+++ 5/js/script.js
@@ -3,46 +3,17 @@ $(window).on('load resize', function(){
   var h = $(window).height();
   var w = $(window).width();
   
-  if(w <= 800){
-    $('.gnavi').addClass("gnavi_sp");
-    $(".gnavi_sp").css({
-      "left":"-" + (w + 120) + "px",
-      "width":(w - 120) + "px",
-    });
 
-    $(".sp_menu").click(function(){
-      TweenMax.to('.gnavi', 0.6, {
-        left: "-" + 0 + 'px',
-        ease: Expo.easeOut
-      });
-      $(".sp_menu_close").css({
-        "z-index":"101",
-      });
+  $('.general h2').on('inview', function() {
+    TweenMax.to(this, 0.8, {
+      opacity: 1,
+      ease: Power0.easeIn
     });
-    $(".sp_menu_close").click(function(){
-      TweenMax.to('.gnavi_sp', 0.4, {
-        left: "-" + (w + 120) + 'px',
-        ease: Expo.easeIn
-      });
-      $(".sp_menu_close").css({
-        "z-index":"100",
-      });
+    TweenMax.to($(this).find('span'), 0.8, {
+      marginLeft: 0,
+      ease: Power1.easeOut
     });
-    $(".nlink a").click(function(){
-      TweenMax.to('.gnavi_sp', 0.4, {
-        left: "-" + (w + 120) + 'px',
-        ease: Expo.easeIn
-      });
-      $(".sp_menu_close").css({
-        "z-index":"100",
-      });
-    });
-  } else {
-    $('.gnavi').removeClass("gnavi_sp");    
-    $(".gnavi").css({
-      "left":"0",
-      "width":"560px",
-    });
-  }
+  });
+
 
 });
