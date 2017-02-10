6bae8c0dc8271ad146b71590ccd344b9d7a21e64
muranaka <formail@java9.com>
2017-02-10 16:47:17 +0900

    4715

--- 5/js/script.js
+++ 5/js/script.js
@@ -5,11 +5,14 @@ $(window).on('load resize', function(){
   
 
   $('.general h2').on('inview', function() {
-    TweenMax.to(this, 0.8, {
+    TweenMax.to(this, 0.6, {
       opacity: 1,
+      marginTop: 0,
       ease: Power0.easeIn
     });
     TweenMax.to($(this).find('span'), 0.8, {
+      delay: 0.5,
+      opacity: 1,
       marginLeft: 0,
       ease: Power1.easeOut
     });
